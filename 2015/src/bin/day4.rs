60af8751348fbf2f1185a42eaa407427cf8d2271
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2018-11-05 13:04:33 +0000

    Day 4

---
+++ 2015/src/bin/day4.rs
@@ -0,0 +1,21 @@
+use aoc2015::*;
+use md5;
+use std::io::Result;
+
+fn run(input: &str, mstr: &str) -> usize {
+    for v in 0.. {
+        let target = format!("{}{}", input, v);
+        let digest = format!("{:x}", md5::compute(target));
+        if digest[0..mstr.len()] == *mstr {
+            return v;
+        }
+    }
+    unreachable!()
+}
+
+fn main() -> Result<()> {
+    let input = read_input(4)?.trim().to_owned();
+    println!("Part 1: {}", run(&input, "00000"));
+    println!("Part 2: {}", run(&input, "000000"));
+    Ok(())
+}
