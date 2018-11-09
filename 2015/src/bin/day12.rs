e2b78ca227a999a9c3d0ac8eddf3df68d6f2c4a5
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2018-11-09 17:23:35 +0000

    Lift red for funsies in day12

--- 2015/src/bin/day12.rs
+++ 2015/src/bin/day12.rs
@@ -9,16 +9,15 @@ fn part1(input: &Value) -> f64 {
     }
 }
 
-fn part2(input: &Value) -> f64 {
-    let red = Value::String("red".to_owned());
+fn part2(red: &Value, input: &Value) -> f64 {
     match input {
         Value::Number(n) => n.as_f64().unwrap(),
-        Value::Array(ref v) => v.iter().map(part2).sum(),
+        Value::Array(ref v) => v.iter().map(|v| part2(red, v)).sum(),
         Value::Object(ref m) => {
-            if m.values().find(|&v| *v == red).is_some() {
+            if m.values().find(|&v| v == red).is_some() {
                 0.0
             } else {
-                m.values().map(part2).sum()
+                m.values().map(|v| part2(red, v)).sum()
             }
         }
         _ => 0.0,
@@ -29,6 +28,7 @@ fn main() -> Result<()> {
     let input = read_input(12)?;
     let input = serde_json::from_str(&input)?;
     println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    let red = Value::String("red".to_owned());
+    println!("Part 2: {}", part2(&red, &input));
     Ok(())
 }
