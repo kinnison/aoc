06de89da9cf5b26a12b1168ed0f05da6c13a1887
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2018-12-11 12:33:10 +0000

    Use Summed-Area-Tables for efficiency

--- 2018/src/bin/day11.rs
+++ 2018/src/bin/day11.rs
@@ -2,6 +2,7 @@ use aoc2018::*;
 
 struct FuelGrid {
     cells: Vec<i8>,
+    sat: Vec<i32>,
     width: usize,
     height: usize,
     serial: usize,
@@ -10,9 +11,12 @@ struct FuelGrid {
 impl FuelGrid {
     fn new(width: usize, height: usize, serial: usize) -> FuelGrid {
         let mut cells = Vec::new();
+        let mut sat = Vec::new();
         cells.resize(width * height, 0);
+        sat.resize((width + 1) * (height + 1), 0);
         FuelGrid {
             cells,
+            sat,
             width,
             height,
             serial,
@@ -21,6 +25,10 @@ impl FuelGrid {
 
     fn celloffset(&self, x: usize, y: usize) -> usize {
         (self.width * y) + x
+    }
+
+    fn satoffset(&self, x: i32, y: i32) -> usize {
+        (x + 1) as usize + ((y + 1) as usize * (self.width + 1))
     }
 
     fn calc_power_level(&self, x: usize, y: usize) -> i8 {
@@ -32,24 +40,39 @@ impl FuelGrid {
         (power as i8) - 5
     }
 
+    fn fill_sat(&mut self, x: i32, y: i32, lvl: i8) {
+        // SAT for x,y is val(x,y)+val(x-1,y)+val(x,y-1)-val(x-1,y-1)
+        let thisidx = self.satoffset(x, y);
+        self.sat[thisidx] = i32::from(lvl)
+            + self.sat[self.satoffset(x - 1, y)]
+            + self.sat[self.satoffset(x, y - 1)]
+            - self.sat[self.satoffset(x - 1, y - 1)];
+    }
+
     fn set_power_levels(&mut self) {
         for y in 0..self.height {
             for x in 0..self.width {
                 let idx = self.celloffset(x, y);
-                self.cells[idx] = self.calc_power_level(x, y);
+                let level = self.calc_power_level(x, y);
+                self.cells[idx] = level;
+                self.fill_sat(x as i32, y as i32, level);
             }
         }
     }
 
     fn score_square(&self, x: usize, y: usize, size: usize) -> i32 {
-        let mut tot: i32 = 0;
-        for x_ in x..x + size {
-            for y_ in y..y + size {
-                let val = self.cells[self.celloffset(x_, y_)];
-                tot += i32::from(val);
-            }
-        }
-        tot
+        let x = x as i32;
+        let y = y as i32;
+        let size = size as i32;
+        // With SAT, the score of a square is the score of the bottom right
+        // plus the score above/left top/left
+        // minus the score above top/right
+        // minus the score left of left/bottom
+        let bottomright = self.sat[self.satoffset(x + size - 1, y + size - 1)];
+        let topperlefter = self.sat[self.satoffset(x - 1, y - 1)];
+        let bottomlefter = self.sat[self.satoffset(x - 1, y + size - 1)];
+        let topperright = self.sat[self.satoffset(x + size - 1, y - 1)];
+        bottomright + topperlefter - bottomlefter - topperright
     }
 
     fn find_best_square(&self, size: usize) -> (usize, usize) {
@@ -70,10 +93,7 @@ impl FuelGrid {
     fn find_best_arbitrary(&self) -> (usize, usize, usize) {
         let mut best = (0, 0, 1);
         let mut bestscore = std::i32::MIN;
-        // Our cutoff here is 30x30 because none of the examples need bigger
-        // than half that, so let's just "hope", we'll likely turn out right
-        for size in 1..=30 {
-            //println!("size: {}", size);
+        for size in 1..=self.width {
             let (x, y) = self.find_best_square(size);
             let score = self.score_square(x, y, size);
             if score > bestscore {
@@ -85,48 +105,70 @@ impl FuelGrid {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    static TEST_INPUT_CELLS: &[(usize, usize, usize, i8)] = &[
-        (3, 5, 8, 4),
-        (122, 79, 57, -5),
-        (217, 196, 39, 0),
-        (101, 153, 71, 4),
-    ];
-
-    static TEST_INPUT_GRIDS: &[(usize, usize, usize, i32)] = &[(18, 33, 45, 29), (42, 21, 61, 30)];
-
-    #[test]
-    fn run_tests() -> Result<()> {
-        for test in TEST_INPUT_CELLS {
-            let grid = FuelGrid::new(300, 300, test.2);
-            assert_eq!(grid.calc_power_level(test.0, test.1), test.3);
+fn test_sat() {
+    let mut grid = FuelGrid::new(5, 5, 5);
+    grid.set_power_levels();
+    println!("Grid:");
+    for y in 0..5 {
+        for x in 0..5 {
+            print!("{:3} ", grid.cells[grid.celloffset(x, y)]);
         }
-        for test in TEST_INPUT_GRIDS {
-            let mut grid = FuelGrid::new(300, 300, test.0);
-            grid.set_power_levels();
-            assert_eq!(test.3, grid.score_square(test.1, test.2, 3));
-            let (x, y) = grid.find_best_square(3);
-            assert_eq!((x, y), (test.1, test.2));
-            assert_eq!(test.3, grid.score_square(x, y, 3));
-        }
-        Ok(())
+        println!();
     }
-
-    static TEST2: &[(usize, usize, usize, usize, i32)] =
-        &[(18, 90, 269, 16, 113), (42, 232, 251, 12, 119)];
-
-    #[test]
-    fn run_tests2() {
-        for test in TEST2 {
-            let mut grid = FuelGrid::new(300, 300, test.0);
-            grid.set_power_levels();
-            let (x, y, size) = grid.find_best_arbitrary();
-            assert_eq!((x, y, size), (test.1, test.2, test.3));
-            assert_eq!(grid.score_square(x, y, size), test.4);
+    println!("Sats:");
+    for y in 0..5 {
+        for x in 0..5 {
+            print!("{:4} ", grid.sat[grid.satoffset(x, y)]);
         }
+        println!();
+    }
+    for y in 0..5 {
+        for x in 0..5 {
+            assert_eq!(
+                grid.score_square(x, y, 1),
+                i32::from(grid.cells[grid.celloffset(x, y)])
+            );
+        }
+    }
+    println!("Score of 3x3 at 1,1 is: {}", grid.score_square(1, 1, 3));
+}
+
+static TEST_INPUT_CELLS: &[(usize, usize, usize, i8)] = &[
+    (3, 5, 8, 4),
+    (122, 79, 57, -5),
+    (217, 196, 39, 0),
+    (101, 153, 71, 4),
+];
+
+static TEST_INPUT_GRIDS: &[(usize, usize, usize, i32)] = &[(18, 33, 45, 29), (42, 21, 61, 30)];
+
+fn run_tests() {
+    println!("Run part 1 tests");
+    for test in TEST_INPUT_CELLS {
+        let grid = FuelGrid::new(300, 300, test.2);
+        assert_eq!(grid.calc_power_level(test.0, test.1), test.3);
+    }
+    for test in TEST_INPUT_GRIDS {
+        let mut grid = FuelGrid::new(300, 300, test.0);
+        grid.set_power_levels();
+        assert_eq!(test.3, grid.score_square(test.1, test.2, 3));
+        let (x, y) = grid.find_best_square(3);
+        assert_eq!((x, y), (test.1, test.2));
+        assert_eq!(test.3, grid.score_square(x, y, 3));
+    }
+}
+
+static TEST2: &[(usize, usize, usize, usize, i32)] =
+    &[(18, 90, 269, 16, 113), (42, 232, 251, 12, 119)];
+
+fn run_tests2() {
+    println!("Run part 2 tests");
+    for test in TEST2 {
+        let mut grid = FuelGrid::new(300, 300, test.0);
+        grid.set_power_levels();
+        let (x, y, size) = grid.find_best_arbitrary();
+        assert_eq!((x, y, size), (test.1, test.2, test.3));
+        assert_eq!(grid.score_square(x, y, size), test.4);
     }
 }
 fn part1(input: usize) -> (usize, usize) {
@@ -142,6 +184,9 @@ fn part2(input: usize) -> (usize, usize, usize) {
 }
 
 fn main() -> Result<()> {
+    test_sat();
+    run_tests();
+    run_tests2();
     let input: usize = read_input(11)?.parse()?;
     println!("Part 1: {:?}", part1(input));
     println!("Part 2: {:?}", part2(input));
