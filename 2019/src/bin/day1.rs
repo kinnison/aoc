a10f6d0360990d2e087fe3cbeb78ab3d9599c338
Daniel Silverstone <dsilvers@digital-scurf.org>
2019-11-30 10:44:17 +0000

    Copy 2018 to 2019, clean up all but day1 ready as a framework

---
+++ 2019/src/bin/day1.rs
@@ -0,0 +1,28 @@
+use aoc2019::*;
+
+fn part2(input: &[i32]) -> Result<i32> {
+    let mut seen: HashSet<i32> = HashSet::new();
+    seen.insert(0);
+    let mut cur = 0;
+    for n in input.iter().cycle() {
+        cur += n;
+        if seen.contains(&cur) {
+            return Ok(cur);
+        }
+        seen.insert(cur);
+    }
+    Err("Unable to find a cycle ever?".into())
+}
+
+fn part1(input: &[i32]) -> i32 {
+    input.iter().sum()
+}
+
+fn main() -> Result<()> {
+    let input = read_input(1)?;
+    let input: Result<Vec<i32>> = input.lines().map(|s| Ok(s.parse()?)).collect();
+    let input = input?;
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input)?);
+    Ok(())
+}
---
+++ 2019/src/lib.rs
@@ -0,0 +1,65 @@
+use std::fs::read_to_string;
+
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub use lazy_static::lazy_static;
+pub use regex::Regex;
+pub use std::cmp::{max, min};
+pub use std::collections::HashMap;
+pub use std::collections::HashSet;
+
+pub use permutohedron::Heap;
+
+pub use serde_json::Value;
+
+pub use chrono::prelude::*;
+
+pub use parsebyregex::ParseByRegex;
+pub use parsebyregex_derive::ParseByRegex;
+
+pub use std::collections::VecDeque;
+
+pub use twoway;
+
+pub fn read_input(day: usize) -> Result<String> {
+    Ok(read_to_string(format!("inputs/day{}", day))?)
+}
+
+pub fn read_input_as_vec<T: ParseByRegex>(day: usize) -> Result<Vec<T>> {
+    let plain = read_input(day)?;
+    Ok(input_as_vec(plain)?)
+}
+
+pub fn input_as_vec<T: ParseByRegex, S: AsRef<str>>(plain: S) -> Result<Vec<T>> {
+    let mapped: Result<Vec<T>> = plain
+        .as_ref()
+        .trim()
+        .lines()
+        .map(ParseByRegex::parse_by_regex)
+        .collect();
+    Ok(mapped?)
+}
+
+pub fn input_as_vec_and_first<T: ParseByRegex, S: AsRef<str>>(
+    plain: S,
+) -> Result<(Vec<T>, String)> {
+    let mut lines = plain.as_ref().trim().lines();
+    let first = lines.next();
+    let mapped: Result<Vec<T>> = lines
+        .filter(|s| !s.trim().is_empty()) // In case there are any blanks after the first line
+        .map(ParseByRegex::parse_by_regex)
+        .collect();
+    if first.is_none() {
+        Err("No lines at all?".into())
+    } else {
+        Ok((
+            mapped?,
+            first.expect("Something went wrong").trim().to_owned(),
+        ))
+    }
+}
+
+pub fn read_input_as_vec_and_first<T: ParseByRegex>(day: usize) -> Result<(Vec<T>, String)> {
+    let plain = read_input(day)?;
+    Ok(input_as_vec_and_first(plain)?)
+}
