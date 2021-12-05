71d3102be825ed4aa278ab27b2c45c502431074d
Daniel Silverstone <dsilvers@digital-scurf.org>
2021-12-05 11:27:28 +0000

    2021: Day 5, at least some optimisation
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- 2021/src/bin/2021day5.rs
+++ 2021/src/bin/2021day5.rs
@@ -10,13 +10,6 @@ struct Vent {
 }
 
 impl Vent {
-    fn minx(&self) -> usize {
-        min(self.x1, self.x2)
-    }
-    fn miny(&self) -> usize {
-        min(self.y1, self.y2)
-    }
-
     fn maxx(&self) -> usize {
         max(self.x1, self.x2)
     }
@@ -27,109 +20,69 @@ impl Vent {
     fn is_orthogonal(&self) -> bool {
         self.x1 == self.x2 || self.y1 == self.y2
     }
-
-    fn point_in_orthogonal_line(&self, x: usize, y: usize) -> bool {
-        (self.x1 == x && (self.miny() <= y && self.maxy() >= y))
-            || (self.y1 == y && (self.minx() <= x && self.maxx() >= x))
-    }
 }
 
 struct OceanFloor {
     vents: Vec<Vent>,
-    minx: usize,
     maxx: usize,
-    miny: usize,
     maxy: usize,
 }
 
 impl From<Vec<Vent>> for OceanFloor {
     fn from(vents: Vec<Vent>) -> Self {
-        let (minx, maxx, miny, maxy) =
-            vents
-                .iter()
-                .copied()
-                .fold((100000, 0, 100000, 0), |mut acc, v| {
-                    acc.0 = min(acc.0, v.minx());
-                    acc.1 = max(acc.1, v.maxx());
-                    acc.2 = min(acc.2, v.miny());
-                    acc.3 = max(acc.3, v.maxy());
-                    acc
-                });
-        OceanFloor {
-            vents,
-            minx,
-            maxx,
-            miny,
-            maxy,
+        let (maxx, maxy) = vents.iter().copied().fold((100000, 0), |mut acc, v| {
+            acc.0 = max(acc.0, v.maxx());
+            acc.1 = max(acc.1, v.maxy());
+            acc
+        });
+        OceanFloor { vents, maxx, maxy }
+    }
+}
+
+impl OceanFloor {
+    fn make_grid(&self, all_vents: bool) -> Vec<Vec<usize>> {
+        let mut grid = Vec::new();
+        for _ in 0..=self.maxy {
+            let mut row = Vec::new();
+            row.resize(self.maxx + 1, 0usize);
+            grid.push(row);
         }
+
+        for vent in self.vents.iter().filter(|v| all_vents || v.is_orthogonal()) {
+            let mut x = vent.x1;
+            let mut y = vent.y1;
+            let stepx = vent.x1.cmp(&vent.x2);
+            let stepy = vent.y1.cmp(&vent.y2);
+            loop {
+                grid[y][x] += 1;
+                if x == vent.x2 && y == vent.y2 {
+                    break;
+                }
+                match stepx {
+                    Ordering::Less => x += 1,
+                    Ordering::Equal => {}
+                    Ordering::Greater => x -= 1,
+                }
+                match stepy {
+                    Ordering::Less => y += 1,
+                    Ordering::Equal => {}
+                    Ordering::Greater => y -= 1,
+                }
+            }
+        }
+        grid
     }
 }
 
 fn part1(input: &OceanFloor) -> usize {
-    let orthogs: Vec<Vent> = input
-        .vents
-        .iter()
-        .copied()
-        .filter(Vent::is_orthogonal)
-        .collect();
-    let mut crossovers = 0;
-    for x in input.minx..=input.maxx {
-        for y in input.miny..=input.maxy {
-            if orthogs
-                .iter()
-                .filter(|v| v.point_in_orthogonal_line(x, y))
-                .count()
-                > 1
-            {
-                crossovers += 1;
-            }
-        }
-    }
-    crossovers
+    let grid = input.make_grid(false);
+    grid.iter()
+        .map(|r| r.iter().filter(|n| **n > 1).count())
+        .sum()
 }
 
 fn part2(input: &OceanFloor) -> usize {
-    let mut grid = Vec::new();
-    for _ in 0..=input.maxy {
-        let mut row = Vec::new();
-        row.resize(input.maxx + 1, 0usize);
-        grid.push(row);
-    }
-
-    for vent in &input.vents {
-        let mut x = vent.x1;
-        let mut y = vent.y1;
-        let stepx = vent.x1.cmp(&vent.x2);
-        let stepy = vent.y1.cmp(&vent.y2);
-        loop {
-            grid[y][x] += 1;
-            if x == vent.x2 && y == vent.y2 {
-                break;
-            }
-            match stepx {
-                Ordering::Less => x += 1,
-                Ordering::Equal => {}
-                Ordering::Greater => x -= 1,
-            }
-            match stepy {
-                Ordering::Less => y += 1,
-                Ordering::Equal => {}
-                Ordering::Greater => y -= 1,
-            }
-        }
-    }
-
-    grid.iter().for_each(|row| {
-        row.iter().copied().for_each(|v| {
-            if v == 0 {
-                print!(".");
-            } else {
-                print!("{}", v)
-            }
-        });
-        println!();
-    });
-
+    let grid = input.make_grid(true);
     grid.iter()
         .map(|r| r.iter().filter(|n| **n > 1).count())
         .sum()
