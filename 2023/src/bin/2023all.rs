7192b8d304f9d72af6fd1fd6bf38559b06da94a8
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2023-12-08 12:17:10 +0000

    2023all: Added all target ready for metachallenge

---
+++ 2023/src/bin/2023all.rs
@@ -0,0 +1,81 @@
+use aoc2023::*;
+
+#[path = "2023day1.rs"]
+mod day01;
+#[path = "2023day2.rs"]
+mod day02;
+#[path = "2023day3.rs"]
+mod day03;
+#[path = "2023day4.rs"]
+mod day04;
+#[path = "2023day5.rs"]
+mod day05;
+#[path = "2023day6.rs"]
+mod day06;
+#[path = "2023day7.rs"]
+mod day07;
+#[path = "2023day8.rs"]
+mod day08;
+// #[path = "2023day9.rs"]
+// mod day09;
+// #[path = "2023day10.rs"]
+// mod day10;
+// #[path = "2023day11.rs"]
+// mod day11;
+// #[path = "2023day12.rs"]
+// mod day12;
+// #[path = "2023day13.rs"]
+// mod day13;
+// #[path = "2023day14.rs"]
+// mod day14;
+// #[path = "2023day15.rs"]
+// mod day15;
+// #[path = "2023day16.rs"]
+// mod day16;
+// #[path = "2023day17.rs"]
+// mod day17;
+// #[path = "2023day18.rs"]
+// mod day18;
+// #[path = "2023day19.rs"]
+// mod day19;
+// #[path = "2023day20.rs"]
+// mod day20;
+// #[path = "2023day21.rs"]
+// mod day21;
+// #[path = "2023day22.rs"]
+// mod day22;
+// #[path = "2023day23.rs"]
+// mod day23;
+// #[path = "2023day24.rs"]
+// mod day24;
+// #[path = "2023day25.rs"]
+// mod day25;
+
+fn main() -> Result<()> {
+    day01::main()?;
+    day02::main()?;
+    day03::main()?;
+    day04::main()?;
+    day05::main()?;
+    day06::main()?;
+    day07::main()?;
+    day08::main()?;
+    //day09::main()?;
+    //day10::main()?;
+    //day11::main()?;
+    //day12::main()?;
+    //day13::main()?;
+    //day14::main()?;
+    //day15::main()?;
+    //day16::main()?;
+    //day17::main()?;
+    //day18::main()?;
+    //day19::main()?;
+    //day20::main()?;
+    //day21::main()?;
+    //day22::main()?;
+    //day23::main()?;
+    //day24::main()?;
+    //day25::main()?;
+    Ok(())
+}
