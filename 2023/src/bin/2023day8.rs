aea1f6d4763d330882381a7cf458b0978cacdaf6
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2023-12-08 11:53:08 +0000

    2023day8: p2 too inefficient to find answer

---
+++ 2023/src/bin/2023day8.rs
@@ -0,0 +1,178 @@
+use aoc2023::*;
+
+pub fn main() -> Result<()> {
+    let input = read_input(8)?;
+    let input = parse_map(&input);
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+    Ok(())
+}
+
+#[derive(Debug, ParseByRegex)]
+#[regex = r"^(?P<from>...) = .(?P<left>...), (?P<right>...).$"]
+struct Rule {
+    from: Room,
+    left: Room,
+    right: Room,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+struct Room(u64);
+
+impl std::fmt::Debug for Room {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let ch = [
+            (self.0 >> 16) as u8 as char,
+            ((self.0 >> 8) & 0xFF) as u8 as char,
+            (self.0 & 0xFF) as u8 as char,
+        ];
+        f.debug_tuple("Room").field(&ch).finish()
+    }
+}
+
+impl FromStr for Room {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let ch1 = (s.as_bytes()[0]) as u64;
+        let ch2 = (s.as_bytes()[1]) as u64;
+        let ch3 = (s.as_bytes()[2]) as u64;
+        Ok(Room((ch1 << 16) + (ch2 << 8) + ch3))
+    }
+}
+
+impl Room {
+    #[allow(non_snake_case)]
+    fn AAA() -> Self {
+        Room::from_str("AAA").unwrap()
+    }
+
+    #[allow(non_snake_case)]
+    fn ZZZ() -> Self {
+        Room::from_str("ZZZ").unwrap()
+    }
+
+    fn ends_a(&self) -> bool {
+        (self.0 & 0xFF) == (b'A' as u64)
+    }
+
+    fn ends_z(&self) -> bool {
+        (self.0 & 0xFF) == (b'Z' as u64)
+    }
+}
+
+#[derive(Debug)]
+struct Map {
+    path: String,
+    rules: HashMap<Room, (Room, Room)>,
+}
+
+fn parse_map(input: &str) -> Map {
+    let (rules, path): (Vec<Rule>, _) = input_as_vec_and_first(input).unwrap();
+
+    let rules = rules
+        .into_iter()
+        .map(|rule| (rule.from, (rule.left, rule.right)))
+        .collect();
+
+    Map { path, rules }
+}
+
+fn part1(input: &Map) -> usize {
+    // Route length
+    let mut pos = Room::AAA();
+    let endpos = Room::ZZZ();
+    let mut len = 0;
+    for lr in input.path.chars().cycle() {
+        pos = match lr {
+            'L' => input.rules[&pos].0,
+            'R' => input.rules[&pos].1,
+            _ => unreachable!(),
+        };
+        len += 1;
+        if pos == endpos {
+            break;
+        }
+    }
+
+    len
+}
+
+fn part2(input: &Map) -> u64 {
+    let mut ghosts = input
+        .rules
+        .keys()
+        .copied()
+        .filter(|k| k.ends_a())
+        .sorted()
+        .collect_vec();
+
+    let mut len = 0;
+    let mut path = input.path.chars().cycle();
+
+    while ghosts.iter().any(|r| !r.ends_z()) {
+        let step = path.next().unwrap();
+        ghosts.iter_mut().for_each(|r| {
+            *r = match step {
+                'L' => input.rules[r].0,
+                'R' => input.rules[r].1,
+                _ => unreachable!(),
+            }
+        });
+        len += 1;
+    }
+
+    len
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static TEST_INPUT1: &str = r#"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"#;
+
+    static TEST_INPUT2: &str = r#"LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"#;
+
+    static TEST_INPUT3: &str = r#"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"#;
+
+    #[test]
+    fn testcase1_1() {
+        let input = parse_map(TEST_INPUT1);
+        eprintln!("{input:?}");
+        assert_eq!(part1(&input), 2);
+    }
+
+    #[test]
+    fn testcase1_2() {
+        let input = parse_map(TEST_INPUT2);
+        eprintln!("{input:?}");
+        assert_eq!(part1(&input), 6);
+    }
+
+    #[test]
+    fn testcase2() {
+        let input = parse_map(TEST_INPUT3);
+        assert_eq!(part2(&input), 6);
+    }
+}
